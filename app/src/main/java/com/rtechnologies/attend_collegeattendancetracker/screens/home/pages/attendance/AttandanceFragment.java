75943c347c76ab62f490fc433dabc1c03694e788
Rasid Ekbal <rtechdevlopment123@gmail.com>
2026-07-29 18:38:34 +0530

    implement the attendance fragment layout with a circular progress indicator, subject-wise list, and a month-view calendar, and refactor fragments to use View Binding while removing boilerplate code

--- app/src/main/java/com/rtechnologies/attend_collegeattendancetracker/screens/home/pages/attendance/AttandanceFragment.java
+++ app/src/main/java/com/rtechnologies/attend_collegeattendancetracker/screens/home/pages/attendance/AttandanceFragment.java
@@ -2,65 +2,33 @@ package com.rtechnologies.attend_collegeattendancetracker.screens.home.pages.att
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.rtechnologies.attend_collegeattendancetracker.R;
+import com.rtechnologies.attend_collegeattendancetracker.databinding.FragmentAttandanceBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AttandanceFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AttandanceFragment extends Fragment {
+    FragmentAttandanceBinding binding;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public AttandanceFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AttandanceFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AttandanceFragment newInstance(String param1, String param2) {
-        AttandanceFragment fragment = new AttandanceFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
+
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_attandance, container, false);
+        binding=FragmentAttandanceBinding.inflate(inflater,container,false);
+        return binding.getRoot();
     }
 }
--- app/src/main/java/com/rtechnologies/attend_collegeattendancetracker/screens/home/pages/task/TaskFragment.java
+++ app/src/main/java/com/rtechnologies/attend_collegeattendancetracker/screens/home/pages/task/TaskFragment.java
@@ -8,59 +8,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.rtechnologies.attend_collegeattendancetracker.R;
+import com.rtechnologies.attend_collegeattendancetracker.databinding.FragmentTaskBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TaskFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TaskFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FragmentTaskBinding binding;
 
     public TaskFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TaskFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TaskFragment newInstance(String param1, String param2) {
-        TaskFragment fragment = new TaskFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding=FragmentTaskBinding.inflate(inflater,container,false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task, container, false);
+        return binding.getRoot();
     }
 }
